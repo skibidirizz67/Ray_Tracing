46c7b5ed84beda2138302288da83e47d13705090
skibidirizz67 <gitskibidirizz@proton.me>
2025-09-14 01:18:52 +0300

    camera refactoring

--- src/main.c
+++ src/main.c
@@ -7,43 +7,13 @@
 
 #define SPHERE2H(obj) ((Hittable){(void*)&obj, &hit_sphere})
 
+#define ASPECT_RATIO (16.0 / 9.0)
+#define IMG_WIDTH 800
+
+
 typedef struct {
     double x, y, z;
 } Vec3;
-
-typedef struct {
-    double r, g, b;
-} Col;
-
-typedef struct {
-    Vec3 center;
-    double radius;
-} Sphere;
-
-typedef struct {
-    Vec3 p;
-    Vec3 normal;
-    double t;
-    bool front_face;
-} HitRecord;
-
-typedef struct {
-    Vec3 origin, direction;
-} Ray;
-
-typedef struct {
-    void *data;
-    bool (*hit)(void *data, Ray r, double tmin, double tmax, HitRecord *hrec);
-} Hittable;
-
-typedef struct {
-    Hittable *hittables;
-    size_t size, capacity;
-} HittableList;
-
-double d2r(double deg) {
-    return deg * PI / 180.0;
-}
 
 Vec3 vec3_add(Vec3 a, Vec3 b) {
     return (Vec3){a.x + b.x, a.y + b.y, a.z + b.z};
@@ -94,18 +64,55 @@ double vec3_dot(Vec3 a, Vec3 b) {
     return a.x*b.x + a.y*b.y + a.z*b.z;
 }
 
-Vec3 at(Ray r, double t) {
-    return vec3_add(r.origin, scalar_mult_vec3(t, r.direction));
-}
+
+
+typedef struct {
+    double r, g, b;
+} Col;
 
 Col vec3_to_col(Vec3 v) {
     return (Col){v.x, v.y, v.z};
 }
 
-void set_face_normal(HitRecord *hrec, Ray r, Vec3 outward_normal) {
-    hrec->front_face = vec3_dot(r.direction, outward_normal) < 0;
-    hrec->normal = hrec->front_face? outward_normal : scalar_mult_vec3(-1, outward_normal);
-}
+
+
+typedef struct {
+    Vec3 center;
+    double radius;
+} Sphere;
+
+typedef struct {
+    Vec3 origin, direction;
+} Ray;
+
+
+
+typedef struct {
+    double min, max;
+} Range;
+
+#define range_size(r) (r.max - r.min)
+#define range_contains(r, x) (r.min <= x && x <= r.max)
+#define range_surrounds(r, x) (r.min < x && x < r.max)
+
+
+
+typedef struct {
+    Vec3 p;
+    Vec3 normal;
+    double t;
+    bool front_face;
+} HitRecord;
+
+typedef struct {
+    void *data;
+    bool (*hit)(void *data, Ray r, Range t, HitRecord *hrec);
+} Hittable;
+
+typedef struct {
+    Hittable *hittables;
+    size_t size, capacity;
+} HittableList;
 
 HittableList hlist_init() {
     HittableList hlist = {0};
@@ -142,14 +149,14 @@ void hlist_clear(HittableList *hlist) {
 void hlist_destroy(HittableList *hlist) {
     free(hlist);
 }
-bool hlist_hitall(HittableList *hlist, Ray r, double tmin, double tmax, HitRecord *hrec) {
+bool hlist_hitall(HittableList *hlist, Ray r, Range t, HitRecord *hrec) {
     HitRecord temp_rec = {0};
     bool hit_any = false;
-    double closest = tmax;
+    double closest = t.max;
 
     for (size_t i = 0; i < hlist->size; i++) {
         if (hlist->hittables[i].data == NULL) continue;
-        if (hlist->hittables[i].hit(hlist->hittables[i].data, r, tmin, closest, &temp_rec)) {
+        if (hlist->hittables[i].hit(hlist->hittables[i].data, r, (Range){t.min, closest}, &temp_rec)) {
             hrec = &temp_rec;
             hit_any = true;
             closest = temp_rec.t;
@@ -157,6 +164,63 @@ bool hlist_hitall(HittableList *hlist, Ray r, double tmin, double tmax, HitRecor
     }
 
     return hit_any;
+}
+
+
+
+typedef struct {
+    double aspect_ratio;
+    int img_width, img_height;
+    Vec3 center, pixel00_loc;
+    Vec3 pixel_delta_u, pixel_delta_v;
+} Camera;
+
+Camera cam_init() {
+    Camera cam = {0};
+
+    cam.aspect_ratio = ASPECT_RATIO;
+    cam.img_width = IMG_WIDTH;
+
+    cam.img_height = (int)(cam.img_width / cam.aspect_ratio);
+    if (cam.img_height < 1) cam.img_height = 1;
+
+    cam.center = (Vec3){0.0, 0.0, 0.0};
+
+    double focal_len = 1.0;
+    double viewport_height = 2.0;
+    double viewport_width = viewport_height * ((double)cam.img_width / cam.img_height);
+
+    Vec3 viewport_u = (Vec3){viewport_width, 0.0, 0.0};
+    Vec3 viewport_v = (Vec3){0.0, -viewport_height, 0.0};
+
+    cam.pixel_delta_u = vec3_div_scalar(viewport_u, cam.img_width);
+    cam.pixel_delta_v = vec3_div_scalar(viewport_v, cam.img_height);
+
+    Vec3 viewport_upper_left = vec3_sub(
+        vec3_sub(
+            vec3_sub(cam.center, (Vec3){0.0, 0.0, focal_len}),
+            vec3_div_scalar(viewport_u, 2)
+        ),
+        vec3_div_scalar(viewport_v, 2)
+    );
+    cam.pixel00_loc = vec3_add(
+        viewport_upper_left,
+        scalar_mult_vec3(0.5, vec3_add(cam.pixel_delta_u, cam.pixel_delta_v))
+    );
+
+    return cam;
+}
+
+Col ray_color(Ray r, HittableList *hlist) {
+    HitRecord hrec;
+    if (hlist_hitall(hlist, r, (Range){0, INFINITY}, &hrec)) {
+        return vec3_to_col(scalar_mult_vec3(
+            0.5,
+            vec3_add_scalar(hrec.normal, 1)
+        )); 
+    }
+
+    return (Col){0};
 }
 
 void print_progress(int bar_width, int progress, int total) {
@@ -183,7 +247,46 @@ void write_pixel(Col pixel) {
     printf("%d %d %d\n", rb, gb, bb);
 }
 
-bool hit_sphere(void *data, Ray r, double tmin, double tmax, HitRecord *hrec) {
+void render(Camera *cam, HittableList *hlist) {
+    printf("P3\n%d %d\n255\n", cam->img_width, cam->img_height);
+
+    for (int y = 0; y < cam->img_height; y++) {
+        print_progress(25, y, cam->img_height);
+        for (int x = 0; x < cam->img_width; x++) {
+            Vec3 pixel_center = vec3_add(
+                vec3_add(
+                    cam->pixel00_loc,
+                    scalar_mult_vec3(x, cam->pixel_delta_u)
+                ),
+                scalar_mult_vec3(y, cam->pixel_delta_v)
+            );
+            Vec3 ray_direction = vec3_sub(pixel_center, cam->center);
+            Ray r = (Ray){cam->center, ray_direction};
+
+            Col pixel_color = ray_color(r, hlist);
+            write_pixel(pixel_color);
+        }
+    }
+
+    fprintf(stderr, "\nDone.\n");
+}
+
+
+
+double d2r(double deg) {
+    return deg * PI / 180.0;
+}
+
+Vec3 at(Ray r, double t) {
+    return vec3_add(r.origin, scalar_mult_vec3(t, r.direction));
+}
+
+void set_face_normal(HitRecord *hrec, Ray r, Vec3 outward_normal) {
+    hrec->front_face = vec3_dot(r.direction, outward_normal) < 0;
+    hrec->normal = hrec->front_face? outward_normal : scalar_mult_vec3(-1, outward_normal);
+}
+
+bool hit_sphere(void *data, Ray r, Range t, HitRecord *hrec) {
     Sphere *sphere = (Sphere*)data;
 
     double a = vec3_len_pow2(r.direction);
@@ -197,9 +300,9 @@ bool hit_sphere(void *data, Ray r, double tmin, double tmax, HitRecord *hrec) {
     double sqrtd = sqrt(discriminant);
 
     double root = (h - sqrtd) / a;
-    if (root <= tmin || tmax <= root) {
+    if (!range_surrounds(t, root)) {
         root = (h + sqrtd) / a;
-        if (root <= tmin || tmax <= root) return false;
+        if (!range_surrounds(t, root)) return false;
     }
 
     hrec->t = root;
@@ -210,78 +313,12 @@ bool hit_sphere(void *data, Ray r, double tmin, double tmax, HitRecord *hrec) {
     return true;
 }
 
-Col ray_color(Ray r, HittableList *hlist) {
-    HitRecord hrec;
-    if (hlist_hitall(hlist, r, 0, INFINITY, &hrec)) {
-        return vec3_to_col(scalar_mult_vec3(
-            0.5,
-            vec3_add_scalar(hrec.normal, 1)
-        )); 
-    }
-
-    return (Col){0};
-    /*Vec3 unit_direction = vec3_normalize(r.direction);
-    double a = 0.5*(unit_direction.y + 1.0);
-    return vec3_to_col(vec3_add(
-        scalar_mult_vec3(1.0-a, (Vec3){1.0, 1.0, 1.0}),
-        scalar_mult_vec3(a, (Vec3){0.5, 0.7, 1.0})
-    ));*/
-}
-
 int main() {
-    double aspect_ratio = 16.0 / 9.0;
-    int img_width = 800;
-
-    int img_height = (int)(img_width / aspect_ratio);
-    if (img_height < 1) img_height = 1;
-
     HittableList hlist = hlist_init();
     hlist_push(&hlist, SPHERE2H(((Sphere){(Vec3){0, 0, -1}, 0.5})));
 
-    double focal_len = 1.0;
-    double viewport_height = 2.0;
-    double viewport_width = viewport_height * ((double)img_width / img_height);
-    Vec3 camera_center = (Vec3){0.0, 0.0, 0.0};
-
-    Vec3 viewport_u = (Vec3){viewport_width, 0.0, 0.0};
-    Vec3 viewport_v = (Vec3){0.0, -viewport_height, 0.0};
-
-    Vec3 pixel_delta_u = vec3_div_scalar(viewport_u, img_width);
-    Vec3 pixel_delta_v = vec3_div_scalar(viewport_v, img_height);
-
-    Vec3 viewport_upper_left = vec3_sub(
-        vec3_sub(
-            vec3_sub(camera_center, (Vec3){0.0, 0.0, focal_len}),
-            vec3_div_scalar(viewport_u, 2)
-        ),
-        vec3_div_scalar(viewport_v, 2)
-    );
-    Vec3 pixel00_loc = vec3_add(
-        viewport_upper_left,
-        scalar_mult_vec3(0.5, vec3_add(pixel_delta_u, pixel_delta_v))
-    );
-
-    printf("P3\n%d %d\n255\n", img_width, img_height);
-
-    for (int y = 0; y < img_height; y++) {
-        print_progress(25, y, img_height);
-        for (int x = 0; x < img_width; x++) {
-            Vec3 pixel_center = vec3_add(
-                vec3_add(
-                    pixel00_loc,
-                    scalar_mult_vec3(x, pixel_delta_u)
-                ),
-                scalar_mult_vec3(y, pixel_delta_v)
-            );
-            Vec3 ray_direction = vec3_sub(pixel_center, camera_center);
-            Ray r = (Ray){camera_center, ray_direction};
-
-            Col pixel_color = ray_color(r, &hlist);
-            write_pixel(pixel_color);
-        }
-    }
-
-    fprintf(stderr, "\nDone.\n");
+    Camera cam = cam_init();
+    render(&cam, &hlist);
     
     return 0;
 }
